d6a641507b59fd8ee104ba86345a78cc741d62b3
ChrisKotKot <101499338+ChrisKotKot@users.noreply.github.com>
2023-04-28 00:26:50 +0200

    Update SpaList.js

--- it-spa/src/views/SpaList.js
+++ it-spa/src/views/SpaList.js
@@ -5,27 +5,28 @@ import { NavButton } from '../common/NavButton';
 import { cartManager } from '../cart/cart-manager';
 
 export function SpaList() {
+	const section = document.createElement('section');
+	const ul = document.createElement('ul');
 
-  const section = document.createElement('section');
-  const ul = document.createElement('ul');
-  
-  section.innerHTML = `
-    <h2>Lista zabiegów SPA</h2>
-    <p>Sprawdź ofertę naszych zabiegów.</p>
+	section.innerHTML = `
+    <h2 class="text-center>"Lista zabiegów SPA</h2>
+    <p class="text-center">Sprawdź ofertę naszych zabiegów.</p>
     <p class="loading">Ładuję listę zabiegów...</p>
+    <div class="spa-list"></div>
   `;
 
-  // pobieramy liste pokoi z serwera
-  fetch('http://localhost:3000/treatments')
-    .then(response => response.json())
-    .then(treatments => {
-        const lis = treatments.map(treatment => {
-          const li = document.createElement('li');
+	// pobieramy liste pokoi z serwera
+	fetch('http://localhost:3000/treatments')
+		.then((response) => response.json())
+		.then((treatments) => {
+			const lis = treatments.map((treatment) => {
+				const type = 'spaList';
+				const li = document.createElement('li');
 
-          li.innerHTML = `
+				li.innerHTML = `
             <h4>${treatment.name}</h4>
             <p>
-            <strong>Place: ${treatment.area}</strong>
+            <strong>Miejsce: ${treatment.area}</strong>
             </p>
             <p>
               <strong>${treatment.price.toFixed(2)} PLN</strong>
@@ -33,24 +34,30 @@ export function SpaList() {
             <footer></footer>
           `;
 
-          const addToCartButton = document.createElement('button');
-          addToCartButton.innerText = 'Dodaj do koszyka';
-          addToCartButton.classList.add('btn');
-          addToCartButton.addEventListener('click', () => cartManager.addItem(treatment));
-          const detailsButton = NavButton('Zobacz więcej...', () => SpaDetails(treatment.id), ['btn']);
-          
-          li.querySelector('footer').append(addToCartButton, detailsButton);
+				const addToCartButton = document.createElement('button');
+				addToCartButton.innerText = 'Dodaj do koszyka';
+				addToCartButton.classList.add('btn');
+				addToCartButton.addEventListener('click', () =>
+					cartManager.addItem(treatment)
+				);
+				const detailsButton = NavButton(
+					'Zobacz więcej...',
+					() => SpaDetails(treatment.id),
+					['btn']
+				);
 
-          return li;
-        });
+				li.querySelector('footer').append(addToCartButton, detailsButton);
 
-        ul.append(...lis);
+				return li;
+			});
 
-        // usuwamy element mowiacy o ladowaniu
-        section.querySelector('.loading').remove();
-        // podstawiamy gotowa liste z pokojami
-        section.append(ul);
-    });
+			ul.append(...lis);
 
-  return section;
+			// usuwamy element mowiacy o ladowaniu
+			section.querySelector('.loading').remove();
+			// podstawiamy gotowa liste z pokojami
+			section.querySelector('.spa-list').append(ul);
+		});
+
+	return section;
 }
